b91d5d33ff51b938a4b3987bba2298f95f2e3964
alvaro <asantosq1@gmail.com>
2018-08-03 12:40:06 +0200

    Uploaded service implementation

--- src/main/java/com/ust/catastro/services/CatastroDetailService.java
+++ src/main/java/com/ust/catastro/services/CatastroDetailService.java
@@ -1,7 +1,9 @@
 package com.ust.catastro.services;
 
+import com.ust.catastro.model.CatastroDetail;
+
 public interface CatastroDetailService {
 
-	
+	public CatastroDetail findCadastreByRefCatastro(String refCatastro);
 	
 }
--- src/main/java/com/ust/catastro/services/impl/CatastroDetailServiceImpl.java
+++ src/main/java/com/ust/catastro/services/impl/CatastroDetailServiceImpl.java
@@ -1,5 +1,29 @@
 package com.ust.catastro.services.impl;
 
-public class CatastroDetailServiceImpl {
+import org.springframework.stereotype.Service;
 
+import com.ust.catastro.clients.CatastroDetailClient;
+import com.ust.catastro.model.CatastroDetail;
+import com.ust.catastro.services.CatastroDetailService;
+import com.ust.catastro.services.converters.CatastroSoapToCatastroDetail;
+
+import lombok.RequiredArgsConstructor;
+
+
+@Service
+@RequiredArgsConstructor
+public class CatastroDetailServiceImpl implements CatastroDetailService{
+
+	private final CatastroDetailClient catastroDetailClient;
+	private final CatastroSoapToCatastroDetail catastroSoapToCatastroDetail;
+	@Override
+	public CatastroDetail findCadastreByRefCatastro(String refCatastro) {
+		
+		CatastroDetail catastroDetail = new CatastroDetail();
+		catastroDetailClient.getCatstroDetail(refCatastro);
+//		catastroDetail = catastroSoapToCatastroDetail.convert();
+		
+		return catastroDetail;
+	}
+	
 }
